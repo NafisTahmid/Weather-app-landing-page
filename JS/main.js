f5a8f76a4ad61779badf145a50940d2fe4694ef5
unknown <nafistahmid4@gmail.com>
2024-08-14 12:54:53 +0600

    New commit

--- JS/main.js
+++ JS/main.js
@@ -9,3 +9,13 @@ function hideSidebar() {
     let sidebar = document.querySelector(".side-bar");
     sidebar.style.display = "none";
 }
+
+
+$( document ).ready(function() {
+    $('.slider-section').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots:false
+    })
+});
